8275b576aa96cebf5d3591319f7bf654aebddf06
Abhishek Agrawal <aagrawal207@gmail.com>
2018-04-06 00:53:04 +0530

    Uploading and downloading are working

--- FTP.java
+++ FTP.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
@@ -12,8 +11,12 @@ import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.*;
+import java.net.*;
+import java.io.*;
 
 public class FTP extends Application {
+    public static String serverAddress = "172.16.185.123";
+    public static int portForCommands = 1234, portForFiles = 2345;
     private static ArrayList<String> listOfFiles = new ArrayList<>();
     private static final int WINDOW_HEIGHT = 650;
     private static final int WINDOW_WIDTH = 900;
@@ -22,22 +25,21 @@ public class FTP extends Application {
     private static final String UPLOAD_BUTTON_STYLE = "-fx-background-color: white; -fx-background-radius: 30; -fx-background-insets: 0;-fx-text-fill: white; -fx-font : 25 arial;-fx-text-fill : linear-gradient(#ff5400, #be1d00);-fx-font-weight: bold;";
     private static final String UPLOAD_TEXT_STYLE = "-fx-font-size: 18px; -fx-font-weight: bold;-fx-text-fill: #541313; -fx-effect: dropshadow( gaussian , rgba(255,255,255,0.5) , 0,0,0,1 ); ";
     private static final String BROWSE_BUTTON_STYLE = "-fx-background-color: white; -fx-background-radius: 6; -fx-background-insets: 0; -fx-font : 18 arial;-fx-text-fill : linear-gradient(#ff5400, #be1d00);-fx-font-weight: bold;";
-    
-    public static void main(String[] args) {
-        listOfFiles.add("Harry potter.mp4");
-        listOfFiles.add(" GTA 5.exe");
-        listOfFiles.add(" Simpsons.mp4");
-        listOfFiles.add(" F.R.I.E.N.D.S.zip");
-        listOfFiles.add(" Batman.zip");
-        listOfFiles.add(" WWE.zip ");
-        listOfFiles.add(" Dark Secrets.zip");
-        listOfFiles.add(" Big Data.zip");
-        listOfFiles.add(" XXX-Sunny Leone.mp4");
-        listOfFiles.add(" Amanda Cerny Vines.mp4");
-        listOfFiles.add(" BigTits.mp4");
+
+    public static void main(String[] args) throws Exception {
+        Socket getMyFileNamesServer = new Socket(InetAddress.getByName(serverAddress), portForCommands);
+        BufferedReader br = new BufferedReader(new InputStreamReader(getMyFileNamesServer.getInputStream()));
+        OutputStream os = getMyFileNamesServer.getOutputStream();
+        os.write("1\n".getBytes());
+        String[] fileNames = br.readLine().split("#%splitter%#");
+        for (String fileName : fileNames)
+            listOfFiles.add(fileName);
+        os.close();
+        br.close();
+        getMyFileNamesServer.close();
         launch(args);
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
         int n = listOfFiles.size();
@@ -46,17 +48,43 @@ public class FTP extends Application {
         Label[] labels = new Label[n];
         GridPane hbox = new GridPane();
         VBox vbox = new VBox();
-        
+
         for (int i = 0; i < n; i++) {
             hbox.setPadding(new Insets(10, 10, 10, 10));
-            labels[i] = new Label(i + 1 + ". " + listOfFiles.get(i));
+            labels[i] = new Label(listOfFiles.get(i));
             labels[i].setStyle(LABEL_STYLE);
             hbox.add(labels[i], 0, 2 * i, 2, 2);
-            
+
             buttons[i] = new Button("Download");
             buttons[i].setStyle(BUTTON_STYLE);
             buttons[i].setLayoutX(WINDOW_WIDTH / 2);
-            
+            buttons[i].addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                try {
+                    Socket getMyFileNamesServer = new Socket(InetAddress.getByName(serverAddress), portForCommands);
+                    OutputStream os = getMyFileNamesServer.getOutputStream();
+                    os.write("2\n".getBytes());
+                    os.write(("one.txt" + "\n").getBytes());
+                    os.close();
+                    getMyFileNamesServer.close();
+                    Socket getMyFileServer = new Socket(InetAddress.getByName(serverAddress), portForFiles);
+                    BufferedReader br = new BufferedReader(new InputStreamReader(getMyFileServer.getInputStream()));
+                    Writer writer = new FileWriter("./Client_Files/" + "one.txt");
+                    String line;
+                    while (true) {
+                        line = br.readLine();
+                        if (line.equals("*#*#*#*EOF*#*#*#*"))
+                            break;
+                        writer.write(line + "\n");
+                    }
+                    writer.flush();
+                    writer.close();
+                    br.close();
+                    getMyFileServer.close();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+
             hbox.add(buttons[i], 2, 2 * i, 2, 2);
             hbox.setHgap(100);
             hbox.setVgap(20);
@@ -67,7 +95,52 @@ public class FTP extends Application {
         scroll.setPrefWidth(WINDOW_WIDTH);
         scroll.setPadding(new Insets(0, 0, 0, 100));
         scroll.setStyle("-fx-background-color: linear-gradient(#ff5400, #be1d00);");
-        
+
+        TextField filename = new TextField("Enter file's path to be uploaded");
+        filename.setPrefWidth(450);
+        filename.setStyle(UPLOAD_TEXT_STYLE);
+        filename.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            DropShadow shadow = new DropShadow();
+            filename.setEffect(shadow);
+            if (filename.getText().contains("Enter file")) {
+                filename.setText("");
+            }
+        });
+        filename.addEventHandler(MouseEvent.MOUSE_EXITED, event -> {
+            DropShadow shadow = new DropShadow();
+            filename.setEffect(shadow);
+            if (filename.getText().compareTo("") == 0) {
+                filename.setText("Enter file's path to be uploaded");
+            }
+        });
+
+        Button browse = new Button("Browse");
+        browse.setStyle(BROWSE_BUTTON_STYLE);
+        browse.setOnAction((ActionEvent e) -> {
+            FileChooser fc = new FileChooser();
+            File file = fc.showOpenDialog(null);
+            String path = file.getAbsolutePath();
+            path = path.replace("\\", "/");
+            filename.setText(path);
+        });
+        browse.setTranslateY(0.4);
+
+        browse.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> {
+            DropShadow shadow = new DropShadow();
+            browse.setEffect(shadow);
+        });
+        browse.addEventHandler(MouseEvent.MOUSE_EXITED, event -> browse.setEffect(null));
+
+        browse.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
+            browse.setTranslateY(2);
+            browse.setTranslateX(-1);
+        });
+
+        browse.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
+            browse.setTranslateY(-2);
+            browse.setTranslateX(1);
+        });
+
         Button upload = new Button("Upload");
         upload.setStyle(UPLOAD_BUTTON_STYLE);
         upload.setOnAction((ActionEvent e) -> {
@@ -83,13 +156,9 @@ public class FTP extends Application {
                     return null;
                 }
             };
-            
             pForm.activateProgressBar(task);
-            
             task.setOnSucceeded(event -> pForm.getDialogStage().close());
-            
             pForm.getDialogStage().show();
-            
             Thread thread = new Thread(task);
             thread.start();
         });
@@ -109,64 +178,43 @@ public class FTP extends Application {
         upload.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
             upload.setTranslateY(-2);
             upload.setTranslateX(1);
-        });
-        
-        TextField filename = new TextField("Enter file's path to be uploaded");
-        filename.setPrefWidth(450);
-        filename.setStyle(UPLOAD_TEXT_STYLE);
-        filename.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-            DropShadow shadow = new DropShadow();
-            filename.setEffect(shadow);
-            if (filename.getText().contains("Enter file")) {
-                filename.setText("");
+            String path = filename.getText();
+            File file = new File(path);
+            String fileName = file.getName();
+            try {
+                Socket getMyFileNamesServer = new Socket(InetAddress.getByName(serverAddress), portForCommands);
+                OutputStream os = getMyFileNamesServer.getOutputStream();
+                os.write("3\n".getBytes());
+                os.write((fileName + "\n").getBytes());
+                os.close();
+                getMyFileNamesServer.close();
+                Socket getMyFileServer = new Socket(InetAddress.getByName(serverAddress), portForFiles);
+                os = getMyFileServer.getOutputStream();
+                BufferedReader fileReader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = fileReader.readLine()) != null)
+                    os.write((line + "\n").getBytes());
+                os.write("*#*#*#*EOF*#*#*#*\n".getBytes());
+                os.flush();
+                fileReader.close();
+                os.close();
+                getMyFileServer.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
             }
         });
-        filename.addEventHandler(MouseEvent.MOUSE_EXITED, event -> {
-            DropShadow shadow = new DropShadow();
-            filename.setEffect(shadow);
-            if (filename.getText().compareTo("") == 0) {
-                filename.setText("Enter file's path to be uploaded");
-            }
-        });
-        
-        Button browse = new Button("Browse");
-        browse.setStyle(BROWSE_BUTTON_STYLE);
-        browse.setOnAction((ActionEvent e) -> {
-            FileChooser fc = new FileChooser();
-            File file = fc.showOpenDialog(null);
-            String path = file.getAbsolutePath();
-            path = path.replace("\\", "/");
-            filename.setText(path);
-        });
-        browse.setTranslateY(0.4);
-        
-        browse.addEventHandler(MouseEvent.MOUSE_ENTERED, event -> {
-            DropShadow shadow = new DropShadow();
-            browse.setEffect(shadow);
-        });
-        browse.addEventHandler(MouseEvent.MOUSE_EXITED, event -> browse.setEffect(null));
-        
-        browse.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-            browse.setTranslateY(2);
-            browse.setTranslateX(-1);
-        });
-        
-        browse.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
-            browse.setTranslateY(-2);
-            browse.setTranslateX(1);
-        });
-    
+
         HBox footerPane = new HBox(40);
         footerPane.setStyle("-fx-background-color: linear-gradient(#ff5400,#ff5400,#be1d00);");
         footerPane.setPrefHeight(WINDOW_HEIGHT / 4);
         footerPane.setPrefWidth(WINDOW_WIDTH);
         footerPane.setPadding(new Insets(50, 50, 50, 100));
-        footerPane.getChildren().addAll(upload, filename, browse);
-        
+        footerPane.getChildren().addAll(filename, browse, upload);
+
         VBox rootPane = new VBox();
         rootPane.setStyle("-fx-background-color : blue");
         rootPane.getChildren().addAll(scroll, footerPane);
-        
+
         primaryStage.setTitle(" Client Machine ");
         primaryStage.setScene(new Scene(rootPane, 900, 650));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("iconFile.png")));
@@ -177,33 +225,33 @@ public class FTP extends Application {
 class ProgressForm {
     private final Stage dialogStage;
     private final ProgressBar pb = new ProgressBar();
-//    private final ProgressIndicator pin = new ProgressIndicator(); // Circle progress bar includes the % indicator
-    
+    //    private final ProgressIndicator pin = new ProgressIndicator(); // Circle progress bar includes the % indicator
+
     public ProgressForm(String title) {
         dialogStage = new Stage();
         dialogStage.initStyle(StageStyle.UTILITY);
         dialogStage.setResizable(false);
         dialogStage.initModality(Modality.APPLICATION_MODAL); // Blocks any interaction with the main window until dialog box is dealt with
-        
+
         pb.setProgress(-1F);
-//        pin.setProgress(-1F);
-        
+        //        pin.setProgress(-1F);
+
         final HBox hb = new HBox();
         hb.setSpacing(5);
         hb.setAlignment(Pos.CENTER);
         hb.getChildren().addAll(pb);
-        
+
         Scene scene = new Scene(hb, 200, 100);
         dialogStage.setScene(scene);
         dialogStage.setTitle(title);
     }
-    
+
     public void activateProgressBar(final Task<?> task) {
         pb.progressProperty().bind(task.progressProperty());
-//        pin.progressProperty().bind(task.progressProperty());
+        //        pin.progressProperty().bind(task.progressProperty());
         dialogStage.show();
     }
-    
+
     public Stage getDialogStage() {
         return dialogStage;
     }
--- FTP_Server/Server.java
+++ FTP_Server/Server.java
@@ -15,47 +15,52 @@ public class Server {
         }
     }
 }
+
 class ClientThread extends Thread {
     private Socket commandClientSocket;
     ServerSocket socketForFiles;
+
     public ClientThread(Socket commandClientSocket, ServerSocket socketForFiles) {
         this.commandClientSocket = commandClientSocket;
         this.socketForFiles = socketForFiles;
     }
+
     public void run() {
         try {
-            BufferedReader commandReader = new BufferedReader(new InputStreamReader(commandClientSocket.getInputStream()));
+            BufferedReader commandReader = new BufferedReader(
+                    new InputStreamReader(commandClientSocket.getInputStream()));
             OutputStream os = commandClientSocket.getOutputStream();
             String cmd = commandReader.readLine();
             switch (cmd) {
-                case "1": // send all file names
-                    File[] listOfFiles = (new File("./files")).listFiles();
-                    String fileNames = "";
-                    for (File i : listOfFiles)
-                        fileNames += i.getName() + "*#|*#";
-                    os.write(fileNames.getBytes());
+            case "1": // send all file names
+                File[] listOfFiles = (new File("./files")).listFiles();
+                String fileNames = "";
+                for (File i : listOfFiles)
+                    fileNames += i.getName() + "#%splitter%#";
+                fileNames += "\n";
+                os.write(fileNames.getBytes());
+                os.flush();
+                break;
+            case "2": // Send file to client
+                String fileNameToSend = commandReader.readLine();
+                File file = new File("./files/" + fileNameToSend);
+                if (file.exists() && file.isFile()) {
+                    Socket fileSendingSocket = socketForFiles.accept();
+                    sendFile(file, fileSendingSocket);
+                } else {
+                    System.out.println("XXX File does not exist XXX");
+                    os.write("*#*#*#*Error*#*#*#*\n".getBytes());
                     os.flush();
-                    break;
-                case "2": // Send file to client
-                    String fileNameToSend = commandReader.readLine();
-                    File file = new File("./files/" + fileNameToSend);
-                    if (file.exists() && file.isFile()) {
-                        Socket fileSendingSocket = socketForFiles.accept();
-                        sendFile(file, fileSendingSocket);
-                    } else {
-                        System.out.println("XXX File does not exist XXX");
-                        os.write("*#*#*#*Error*#*#*#*\n".getBytes());
-                        os.flush();
-                    }
-                    break;
-                case "3": // Receive file from the client
-                    String fileNameToReceive = commandReader.readLine();
-                    File fileToReceive = new File("./files/" + fileNameToReceive);
-                    Socket fileReceivingSocket = socketForFiles.accept();
-                    receiveFile(fileToReceive, fileReceivingSocket);
-                    break;
-                default:
-                    System.out.println("XXX Wrong code received XXX");
+                }
+                break;
+            case "3": // Receive file from the client
+                String fileNameToReceive = commandReader.readLine();
+                File fileToReceive = new File("./files/" + fileNameToReceive);
+                Socket fileReceivingSocket = socketForFiles.accept();
+                receiveFile(fileToReceive, fileReceivingSocket);
+                break;
+            default:
+                System.out.println("XXX Wrong code received XXX");
             }
             os.close();
             commandReader.close();
@@ -65,23 +70,24 @@ class ClientThread extends Thread {
             ex.printStackTrace();
         }
     }
+
     private static void receiveFile(File fileToReceive, Socket fileReceivingSocket) throws Exception {
-        BufferedReader fileReader = new BufferedReader(new FileReader(fileToReceive));
+        BufferedReader br = new BufferedReader(new InputStreamReader(fileReceivingSocket.getInputStream()));
         fileToReceive.createNewFile();
         String line;
         Writer writer = new FileWriter(fileToReceive);
         while (true) {
-            line = fileReader.readLine();
-            if(line.equals("*#*#*#*EOF*#*#*#*\n"))
+            line = br.readLine();
+            if (line.equals("*#*#*#*EOF*#*#*#*"))
                 break;
             writer.write(line + "\n");
         }
         writer.flush();
         writer.close();
-        fileReader.close();
+        br.close();
         fileReceivingSocket.close();
     }
-    
+
     private static void sendFile(File file, Socket fileSendingSocket) throws Exception {
         OutputStream os = fileSendingSocket.getOutputStream();
         BufferedReader fileReader = new BufferedReader(new FileReader(file));
